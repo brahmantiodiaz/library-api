f3968376cff84bcd950f02d1619a0bfb168cfd90
brahmantiodiaz <auliabrahmantio@gmail.com>
2020-01-11 13:27:59 +0700

    librarti-api

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,41 +1,54 @@
 const express = require('express');
 
 const app = express.Router();
-const repo = require('../repos/LibraryRepo');
+const repository = require('../repos/LibraryRepo');
 
 // TODO: Memanggil fungsi listBuku untuk mendapatkan data semua buku yang ada
 app.get('/', (req, res) => {
-  // isi disini
+  repository.listBuku().then((library) => {
+    res.json(library);
+  }).catch((error) => console.log(error));
 });
 
 // TODO: Memanggil fungsi cariBuku untuk mendapatkan spesifik buku berdasarkan ID
 app.get('/:id', (req, res) => {
   const { id } = req.params;
-  // tambahkan disini
+  repository.cariBuku(id).then((library) => {
+    res.json(library);
+  }).catch((error) => console.log(error));
 });
 
 // TODO: Memanggil fungsi tambahBuku untuk menambah buku baru pada list
 app.post('/', (req, res) => {
-  const infoBukuBaru = req.body;
-  // isi disini
+  const infoBuku = req.body;
+  console.log(infoBuku)
+  repository.tambahBuku(infoBuku).then((library) => {
+    res.json(library);
+  }).catch((error) => console.log(error));
 });
 
 // TODO: Memanggil fungsi hapusBuku untuk menghapus buku tertentu
 app.delete('/:id', (req, res) => {
   const { id } = req.params;
-  // isi disini
+  repository.hapusBuku(id).then((ok) => {
+    console.log(ok);
+    console.log(`Deleted record with id: ${id}`);
+    res.status(200).json([]);
+  }).catch((error) => console.log(error));
 });
 
 // TODO: Memanggil fungsi rubahStatusPeminjaman untuk merubah status peminjaman buku tertentu
 app.put('/:id', (req, res) => {
   const { id } = req.params;
-  // isi disini
+  repository.rubahStatusPeminjaman(id, req.body)
+    .then(res.status(200).json([]))
+    .catch((error) => console.log(error));
 });
 
 // Memanggil fungsi rubahStatusPeminjaman untuk merubah status peminjaman buku tertentu
 app.put('/rubah/:id', (req, res) => {
   const { id } = req.params;
-  repo.rubahInfoBuku(id, req.body)
+  repository.rubahInfoBuku(id, req.body)
     .then((info) => {
       res.status(200).json({
         info: info,
